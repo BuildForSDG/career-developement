b52f4a597c6b1e36a9d524629d4a03ac9b91709e
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-11 20:54:45 +0000

    database connection and registration of a user

--- src/project.php
+++ src/project.php
@@ -11,16 +11,32 @@ class Project
     $database='careerdb';
     
     $dbc=mysqli_connect($servername,$username,$password ,$database);
+    return $dbc;
 
-
-    if ($dbc){
+    /*if ($dbc){
      echo 'yeah';
      }else{
        echo 'Could not connect because:mysqli_connect_error()';
-   }
+   }*/
 
  }
+
+ function registerUser ($dbc,$type_of_user,$password,$location,$business_description,$company,$name,$email,$mobile,$educational_level){
+  
+
+$q="INSERT INTO users (name,type,email,password,mobile,location,business_description,company,educational_level) 
+VALUES ('$name','$type_of_user','$email','$password','$mobile','$location','$business_description','$company','$educational_level') ";
+
+
+   if (mysqli_query($dbc,$q)) {
+      return true;
+   }
+
+
+
 }
 
-$project=new Project;
-$project->connection();
+}
+
+/*$project=new Project;
+$project->connection();*/
