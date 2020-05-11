aec93cd4a335a2e89f53b5cc4c2a41d7468e6761
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-11 18:49:06 +0000

    function to connect to the database

--- src/project.php
+++ src/project.php
@@ -4,4 +4,23 @@ namespace App;
 
 class Project
 {
+    function connection(){
+    $servername = $_SERVER['SERVER_NAME'];
+    $username = "root";
+    $password = "";
+    $database='careerdb';
+    
+    $dbc=mysqli_connect($servername,$username,$password ,$database);
+
+
+    if ($dbc){
+     echo 'yeah';
+     }else{
+       echo 'Could not connect because:mysqli_connect_error()';
+   }
+
+ }
 }
+
+$project=new Project;
+$project->connection();
