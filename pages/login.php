a723d47b21a058a939c45104a74569f95698ea86
Avalon <hopegag@gmail.com>
2020-05-13 22:55:14 +0000

    UI Interface for login and Database query in Login Class

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,27 @@
 <?php
-    include('../includes/header.php')
+
+use App\Login\Login;
+
+    include_once('../config/database.php');
+    include_once('../src/Login/Login.php');
+    include('../includes/header.php');
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $user = new Login($db);
+    if($_POST){
+      $user->email = $_POST['email'];
+      $user->password = $_POST['password'];
+
+      if($user->login()){
+          echo "<div class='alert alert-success'>Login Successful...</div>";
+      }else {
+          echo "<div class='alert alert-danger'>Invalid Login Credentials</div>";
+      }
+  }
+
+    include('../includes/header.php');
 ?>
 <div class="login-box">
   <div class="login-logo">
@@ -11,11 +33,11 @@
 
     <form action="" method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
+        <input type="email" name="email" class="form-control" placeholder="Email">
         <span class="fa fa-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" name="password" class="form-control" placeholder="Password">
         <span class="fa fa-lock form-control-feedback"></span>
       </div>
       <div class="row">
--- src/Login/Login.php
+++ src/Login/Login.php
@@ -8,7 +8,7 @@ class Login
     private $conn;
     private $table_name = 'users';
 
-    public $fullname, $company, $usertype, $email, $password, $phone, $location;
+    public $email, $password;
     public function __construct($db)
     {
         $this->conn = $db;
@@ -19,9 +19,29 @@ class Login
         header("Location: ./pages/login.php");
     }
 
-
-    public function create()
+    /**
+     * Login User
+     *
+     * @return void
+     */
+    public function login()
     {
+        $queryBuilder = $this->conn->createQueryBuilder();
+        $queryBuilder->select('id','email','password')
+                    ->from('users')
+                    ->where('email = ?')
+                    ->andWhere('password = ?')
+                    ->setParameter(0, $this->email)
+                    ->setParameter(1, $this->password);
+
+            $stmt = $queryBuilder->execute();
+            $results = $stmt->fetch();
+
+            if($results){
+                return true;
+            }else {
+                return false;
+            }
 
     }
 }
