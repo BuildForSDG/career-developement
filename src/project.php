54c9d6dcc5b1d891406b8c33eb6e8963d545bb3c
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-14 10:05:12 +0000

    modification of confirmatory link

--- src/project.php
+++ src/project.php
@@ -39,7 +39,7 @@ VALUES ('$name','$type_of_user','$email','$password','$mobile','$location','$bus
   function confirmationMail($email,$name){
    $subject="Account Activated Notification";
    $message="Hello ".$name.",\r\n\r\n"
-      ."Your  account has been created.Click here: ".$_SERVER['SERVER_NAME']."/?"."rand()  to log in.\r\n\r\n";
+      ."Your  account has been created.Click here: ".$_SERVER['SERVER_NAME']."  to log in.\r\n\r\n";
    
 $headers="From: mlsghana <no-reply@".$_SERVER['SERVER_NAME'].">";
 
