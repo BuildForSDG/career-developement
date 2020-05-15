471cf4e4661aa86fe46bf8f3e9ac19e3e45c6a46
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-15 07:47:48 +0000

    js template for pages

---
+++ config/footer1.php
@@ -0,0 +1,12 @@
+<!-- jQuery 3 -->
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
