336e9bdb5b3019f637966e861be1534416ac8f6e
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-15 07:47:24 +0000

    js template for login and registration pages

---
+++ config/footer.php
@@ -0,0 +1,18 @@
+<!-- jQuery 3 -->
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="js/icheck.min.js"></script>
+<script src="js/select.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' /* optional */
+    });
+  });
+</script>
+</body>
+</html>
