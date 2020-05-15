69caf029b223b68ab980af7ce935837cc9e3a7c1
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-15 08:38:45 +0000

    optional dropdown of category

--- js/select.js
+++ js/select.js
@@ -3,7 +3,11 @@ $("select[name=type_of_user]").change(function () {
    
     if(type=='trainee'){
       $("div.educational_level").removeAttr('hidden');
-    }else {
+    }else if(type=='trainer'){
+      $("div.description").removeAttr('hidden');
+      $("div.category").removeAttr('hidden');
+    }
+    else {
         $("div.description").removeAttr('hidden');  
     }
 
