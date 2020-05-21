0379096422bcfe4d1903ce9631b0fb47978dc078
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-05-21 11:33:00 +0000

    view provided for dashboard page

--- config/header1.php
+++ config/header1.php
@@ -17,6 +17,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="css/AdminLTE.min.css">
+  
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect. -->
