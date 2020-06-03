4de5c29581ef02350df012d372c04612173f1afe
Thomas Frimpong <tfrimpong1987@gmail.com>
2020-06-03 12:59:16 +0000

    home page and its style sheet

--- index.php
+++ index.php
@@ -1,1 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+
+	<!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+    <meta name="description" content="">
+    <meta name="author" content="">    
+    <link rel="shortcut icon" href=""> 
+
+    <title>Career Development </title>
+
+    <!-- Material Design Icons -->
+    <!-- <link rel="stylesheet" href="assets/MaterialDesign-Webfont-master/css/materialdesignicons.min.css"> -->
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    
+
+
+    <!-- Fonts CSS -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,200,500,600,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> 
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-fixed-top scrolling-navbar bar-mobi">
+
+	    <div class="container">
+
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	           <a class="navbar-brand" href="index.html">
+                   <!-- <img src="" alt="CDC" class="logo"> -->
+                </a>
+	        </div>
+
+	        <div id="navbar" class="navbar-collapse collapse">
+
+		        <ul class="nav navbar-nav navbar-right kl-nav">
+		            <li class="active"><a href="index.php">Home</a></li>
+		            
+		            <li><a href="register.php">Register</a></li>
+                    <li><a href="login.php">Login</a></li> 
+                     <li><a href="#">Learn More</a></li>   
+		        </ul>
+
+	        </div><!-- /.nav-collapse -->
+
+	    </div>
+
+    </nav><!-- /.Navbar -->
+
+
+    <!-- Mask -->
+    <div class="view hm-black-strong" style="background-image: url('img/office.jpg');">
+
+        <div class="full-bg-img flex-center">
+
+	        <div class="container container-flex">
+
+	        	<div class="col-md-4"></div>
+
+		        <div class="col-md-6 text">
+
+		        	<p>
+		        		<h1 class="hidden-xs">Career Development Center</h1>
+		        	</p>
+
+		        	<p>
+		        		<h4>
+                        Are you eager to kick start your career? 
+    Find institutions that offer the kind of skill training you need. 
+    Also find investors who can help you fund your business.
+		        		</h4>
+		        	</p>
+
+		        	<br>
+
+		        	<p>
+		        		<a href="register.php"><button class="btn btn-info btn-lg text-btn">Register Now!</button></a>
+		        	</p>
+
+		        </div>
+
+		        <div class="col-md-3"></div>
+
+	        </div>
+
+	    </div>
+
+	</div>
+
+
+	<!-- Footer -->
+	<footer class="main-footer mobi-footer">
+
+		<div class="container container-footer">
+
+			<div class="col-md-6 foot-txt">
+				<ul>
+					<li> &copy; <?php print_r(date("Y")); ?> CDC. All Rights Reserved</li>
+				</ul>
+			</div>
+
+			<div class="col-md-6 social">
+
+				<ul>
+					<li><a href=""><span class="mdi mdi-facebook"></span></a></li>
+					<li><a href=""><span class="mdi mdi-twitter"></span></a></li>
+					<li><a href=""><span class="mdi mdi-linkedin"></span></a></li>
+					<li><a href=""><span class="mdi mdi-instagram"></span></a></li>
+				</ul>
+
+			</div>
+
+		</div>
+
+	</footer>
+
+   
+
+
+    <!-- SCRIPTS -->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+</body>
+</html>
